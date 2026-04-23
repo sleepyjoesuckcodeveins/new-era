1546f6414587dd85cd9ff4af3176740cf2db16f0
sleepyjoesuckcodeveins <n.chin@student.fontys.nl>
2026-04-24 01:06:33 +0200

    commit before adding it to gitlab

---
+++ new era web/WebApplication1/Pages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewEra.Domain.Interface;
+using NewEra.Domain.Models;
+using NewEra.BLL;
+using NewEra.Dal;
+
+
+namespace WebApplication1.Pages;
+
+public class IndexModel : PageModel
+{
+     private NeweraProductService productService;
+     public List<Product> Products { get; set; }
+
+    [BindProperty(SupportsGet = true)]
+    public string searchterm { get; set; } = string.Empty;
+
+
+    public IndexModel(NeweraProductService productService)
+    {
+        this.productService = productService;
+    }
+   
+
+    public void OnGet()
+
+    {
+        //this part was done by ai.
+            Products = string.IsNullOrWhiteSpace(searchterm)
+            ? productService.GetAllProducts()
+            : productService.SearchProduct(searchterm);
+    }
+
+
+    public void OnPost()
+    {
+        Products = productService.SearchProduct(searchterm ?? string.Empty);
+    }
+}
