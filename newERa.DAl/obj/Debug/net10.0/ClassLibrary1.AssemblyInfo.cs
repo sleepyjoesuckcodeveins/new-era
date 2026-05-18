63fbb835c241fb133a18a301aaf430c4d1a91bf2
sleepyjoesuckcodeveins <n.chin@student.fontys.nl>
2026-05-18 14:18:57 +0200

    unnessary

--- newERa.DAl/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
+++ newERa.DAl/obj/Debug/net10.0/ClassLibrary1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d462f1c4a4a2ffefc0099f4b7a3ae8d019d04469")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a7067b1a4f80f2c75391523c3b61a2ddfd8756e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
