276ba2779f8193c21fb74560ea771cc7c8f7b5e8
sleepyjoesuckcodeveins <n.chin@student.fontys.nl>
2026-05-18 12:21:04 +0200

    Implement login page changes

--- new era web/WebApplication1/Pages/Login.cshtml.cs
+++ new era web/WebApplication1/Pages/Login.cshtml.cs
@@ -1,10 +1,27 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NewEra.Domain.Interface;
+using NewEra.BLL;
 
 namespace WebApplication1.Pages;
 
 public class LoginModel : PageModel
 {
+    private readonly LoginService _loginService;
+    public LoginModel(LoginService loginService)
+    {
+        _loginService = loginService;
+     }
+
+
+    [BindProperty]
+    public InputModel Input { get; set; }
+     public class InputModel
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }   
+    
     public void OnGet()
     {
     }
