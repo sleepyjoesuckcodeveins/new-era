f921635fe107dbf32199f2c18568efe80de6ceae
sleepyjoesuckcodeveins <n.chin@student.fontys.nl>
2026-05-18 09:52:29 +0200

    ai change the frontend

--- NewEra.Domain/obj/Debug/net10.0/NewEra.Domain.AssemblyInfo.cs
+++ NewEra.Domain/obj/Debug/net10.0/NewEra.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewEra.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+821eed5cb8f97b67fd28cb480809c03ffa1fcb7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b8654be04ff333bd5ad79f498601440080c235")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewEra.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewEra.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- unittesting/UnitTesting/obj/Debug/net10.0/UnitTesting.AssemblyInfo.cs
+++ unittesting/UnitTesting/obj/Debug/net10.0/UnitTesting.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTesting")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21190a0e5ab3c2f27733f2c2e95546d55f2ab482")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22b8654be04ff333bd5ad79f498601440080c235")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTesting")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTesting")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
