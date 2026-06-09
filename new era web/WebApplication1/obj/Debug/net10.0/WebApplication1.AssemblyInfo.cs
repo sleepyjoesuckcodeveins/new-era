97d21dc3ec788ec4c4a5160ce0768ede35f212bc
sleepyjoesuckcodeveins <n.chin@student.fontys.nl>
2026-06-09 13:17:45 +0200

    admin services completed

--- new era web/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ new era web/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6390c63dd0438a14e689381bbf42daf92dc4ec67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b587f473bfa81d46742625044a917885b08703e")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
